8f1a9c2068d4ef9f42ec40ff9453193a1b61bd7c
axel-paillaud <contact@axelweb.fr>
2024-06-17 12:33:41 +0200

    add translations

---
+++ translations/fr.php
@@ -0,0 +1,16 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{tribufaq}prestashop>tribufaq_c888438d14855d7d96a2724ee9c306bd'] = 'Configuration mise à jour';
+$_MODULE['<{tribufaq}prestashop>tribufaq_f4f70727dc34561dfde1a3c529b6205c'] = 'Configuration';
+$_MODULE['<{tribufaq}prestashop>tribufaq_b622458111f525d60fe5cc7486b1c42b'] = 'Nombre de question/réponse à afficher';
+$_MODULE['<{tribufaq}prestashop>tribufaq_7e49dcfc357793a21eacca7009c8c73a'] = 'Seule les nombres sont autorisés';
+$_MODULE['<{tribufaq}prestashop>tribufaq_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{tribufaq}prestashop>admintribufaqcategorycontroller_b068931cc450442b63f5b3d276ea4297'] = 'nom';
+$_MODULE['<{tribufaq}prestashop>admintribufaqcategorycontroller_4d3d769b812b6faa6b76e1a8abaece2d'] = 'Activé';
+$_MODULE['<{tribufaq}prestashop>admintribufaqquestioncontroller_a97ea56b0e00b2379736ae60869ff66a'] = 'Question';
+$_MODULE['<{tribufaq}prestashop>admintribufaqquestioncontroller_3751695c17afed4055a7a5ad5d46f6f5'] = 'Réponse';
+$_MODULE['<{tribufaq}prestashop>admintribufaqquestioncontroller_3adbdb3ac060038aa0e6e6c138ef9873'] = 'Catégorie';
+$_MODULE['<{tribufaq}prestashop>admintribufaqquestioncontroller_3112209b2dd9b55cf5dbc4865dd15afd'] = 'Date création';
+$_MODULE['<{tribufaq}prestashop>admintribufaqquestioncontroller_4d3d769b812b6faa6b76e1a8abaece2d'] = 'Activé';
